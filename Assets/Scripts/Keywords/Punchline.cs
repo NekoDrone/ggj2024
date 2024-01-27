6a03a84b75325a876c394879b11b0ed27159cdc6
Sylfr Serenity Tan <98162476+NekoDrone@users.noreply.github.com>
2024-01-27 22:43:43 +0800

    feat: punchline keyword

---
+++ Assets/Scripts/Keywords/Punchline.cs
@@ -0,0 +1,12 @@
+﻿namespace Keywords
+{
+    public class Punchline : IKeyword
+    {
+        public int PunchlineValue { get; set; } = 0;
+
+        public void ExecuteEffect()
+        {
+            // trigger the Punchline event with the current PunchlineValue
+        }
+    }
+}
