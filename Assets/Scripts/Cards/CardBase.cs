b018249201f343f9bf06d9e183c985f19e36cc4b
Sylfr Serenity Tan <98162476+NekoDrone@users.noreply.github.com>
2024-01-27 22:43:31 +0800

    feat: card base setup

---
+++ Assets/Scripts/Cards/CardBase.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Keywords;
+
+namespace Cards
+{
+    public class CardBase : ICard
+    {
+        public string CardName { get; set; }
+        public int Cost { get; set; }
+        public List<PossibleKeywords> Keywords { get; set; }
+        public CardBase(string cardName, int cost)
+        {
+            this.CardName = cardName;
+            this.Cost = cost;
+            this.Keywords = new List<PossibleKeywords>();
+        }
+
+
+        public void AddKeyword(PossibleKeywords keyword)
+        {
+            this.Keywords.Add(keyword);
+        }
+    }
+}
