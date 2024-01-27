f31f54702bfabec2348d479f7e2318ac900831a8
Sylfr Serenity Tan <98162476+NekoDrone@users.noreply.github.com>
2024-01-28 01:28:08 +0800

    feat: initial base events framework

---
+++ Assets/Scripts/Events/BaseEvents.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Events
+{
+    public static class BaseEvents
+    {
+        public static event Action<int> DealNormalDamage;
+        public static void InvokeDealNormalDamage(int damageValue)
+        {
+            DealNormalDamage?.Invoke(damageValue);
+        }
+
+        public static event Action<int> DealDirectDamage;
+        public static void InvokeDealDirectDamage(int damageValue)
+        {
+            DealDirectDamage?.Invoke(damageValue);
+        }
+
+        public static event Action<int> IncreaseShield;
+        public static void InvokeGainShield(int shieldValue)
+        {
+            IncreaseShield?.Invoke(shieldValue);
+        }
+        
+        public static event Action<int> ReduceShield;
+        public static void InvokeReduceShield(int shieldValue)
+        {
+            ReduceShield?.Invoke(shieldValue);
+        }
+
+    }
+}
