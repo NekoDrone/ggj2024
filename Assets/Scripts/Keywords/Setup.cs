5f8d8c7056003e24366fa62c20c55df0fd3b3d92
Sylfr Serenity Tan <98162476+NekoDrone@users.noreply.github.com>
2024-01-27 22:44:01 +0800

    feat: setup keyword

---
+++ Assets/Scripts/Keywords/Setup.cs
@@ -0,0 +1,17 @@
+﻿namespace Keywords
+{
+    public class Setup : IKeyword
+    {
+        public int SetupValue { get; set; }
+
+        public Setup(int setupValue)
+        {
+            this.SetupValue = setupValue;
+        }
+        
+        public void ExecuteEffect()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
