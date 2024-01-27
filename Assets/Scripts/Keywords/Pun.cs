4f0e6241bab93b1c6efbe0525547113b6c0d1d05
Sylfr Serenity Tan <98162476+NekoDrone@users.noreply.github.com>
2024-01-27 23:12:47 +0800

    feat: pun keyword

---
+++ Assets/Scripts/Keywords/Pun.cs
@@ -0,0 +1,10 @@
+﻿namespace Keywords
+{
+    public class Pun : IKeyword
+    {
+        public void ExecuteEffect()
+        {
+            // TODO: trigger Pun event to deal 1 damage to all enemies
+        }
+    }
+}
